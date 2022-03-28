6681112dfcd45f6dcd31522069005e6f20f5e2b7
mariya0900 <maria.anashkina@ontariotechu.net>
2022-03-27 22:22:03 -0400

    Editable cells done

--- my_jquery.js
+++ my_jquery.js
@@ -1,48 +1,11 @@
-let deselectAll=function(){
-    $(".cell").css("background-color", "white");
-}
-let selectRow=function(index){
-    
-    $(`.row${index}`).css("background-color", "#e0e0ff");
-}
-
-let selectCol=function(index){
-    
-    $(`.col${index}`).css("background-color", "#e0e0ff");
-}
-
-$(document).ready(function(){
-    
-    $(".studentId").click(function(){
-        deselectAll();
-        selectRow($(this).attr('id').substring(3, ));
-        /*if ($(this).attr('id')==='row0'){
-            selectRow(0);
-            $(".row0").css("background-color", "#e0e0ff");
-        }
-        else if ($(this).attr('id')==='row1'){
-            selectRow(1);
-            $(".row1").css("background-color", "#e0e0ff");
-        }
-        else{
-            selectRow(2);
-            $(".row2").css("background-color", "#e0e0ff");
-        }*/
-        
-    });
-    
-    $(".asmt").click(function(){
-        deselectAll();
-        selectCol($(this).attr('id').substring(3, ));
-        /*if ($(this).attr('id')==='col0'){
-            $(".col0").css("background-color", "#e0e0ff");
-        }
-        else if ($(this).attr('id')==='col1'){
-            $(".col1").css("background-color", "#e0e0ff");
-        }
-        else{
-            $(".col2").css("background-color", "#e0e0ff");
-        }
-        */
-    });
-  });
+$(document).ready(function() {
+	$("td").click(function () {
+		$(this).replaceWith("<td id=\"activeTd\"><input id=\"activeInput\" class=\"" + $(this).attr("class") + "\" value=\"" + $(this).html() + "\"></input></td>");
+		$("#activeInput").keypress(function (event) {
+			if (event['code'] == "Enter")
+			{
+				$("#activeTd").replaceWith("<td class=\"" + $("#activeTd").attr("class") + "\">" + $("#activeInput").val() + "</td>");
+			}
+		});
+	});
+});
