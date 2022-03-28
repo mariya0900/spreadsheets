86af598b4023cd59ffd35b2b3cf7feac1e075ad6
mariya0900 <maria.anashkina@ontariotechu.net>
2022-03-27 23:01:37 -0400

    Select row/column + editable cells

--- my_jquery.js
+++ my_jquery.js
@@ -1,4 +1,28 @@
+let deselectAll=function(){
+    $(".cell").css("background-color", "white");
+}
+let selectRow=function(index){
+
+    $(`.row${index}`).css("background-color", "#e0e0ff");
+}
+
+let selectCol=function(index){
+
+    $(`.col${index}`).css("background-color", "#e0e0ff");
+}
 $(document).ready(function() {
+    $(".studentId").click(function(){
+        deselectAll();
+        selectRow($(this).attr('id').substring(3, ));
+        
+
+    });
+
+    $(".asmt").click(function(){
+        deselectAll();
+        selectCol($(this).attr('id').substring(3, ));
+       
+    });
 	$("td").click(function () {
 		$(this).replaceWith("<td id=\"activeTd\"><input id=\"activeInput\" class=\"" + $(this).attr("class") + "\" value=\"" + $(this).html() + "\"></input></td>");
 		$("#activeInput").keypress(function (event) {
