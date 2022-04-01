86e8218b0d18582db81a6c25b9f53243077a993c
mariya0900 <maria.anashkina@ontariotechu.net>
2022-04-01 17:13:19 -0400

    Dynamic table load is working using AJAX

--- my_jquery.js
+++ my_jquery.js
@@ -10,8 +10,47 @@ let selectCol=function(index){
 
     $(`.col${index}`).css("background-color", "#e0e0ff");
 }
+
 $(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8000/grades.csv',
+        dataType: 'text',
+        async: false,
+        success: function(response) {
+            let body = "<table>";
+            let rows=response.split('\n');
+            body += "<tr>";
+            let header=rows[0];
+            let headers=header.split(',');
+            body+='<th>SID</th>';
+            headers.splice(0,1);
+            for (i in headers){
+                body+='<th class="asmt" id="col'+i+'">'+headers[i]+'</th>';
+            }
+            body += "</tr>";
+            
+            rows.splice(0, 1);
+            console.log(rows);
+            for (let i in rows){
+                
+                body+='<tr>';
+                let items=rows[i].split(',');
+                
+                body+='<th class="studentId" id="row'+i+'">'+items[0]+'</th>';
+                items.splice(0,1);
+                for (let j in items){
+                    body+='<td class="cell row'+i+' col'+j+'">'+items[j]+'</td>';
+                }
+                body+='</tr>';
+            }
+            body += "</table>";
+            $('#container').replaceWith(body);
+        }
+    });
+
     $(".studentId").click(function(){
+        alert('studentId clicked');
         deselectAll();
         selectRow($(this).attr('id').substring(3, ));
         
@@ -21,6 +60,7 @@ $(document).ready(function() {
     $(".asmt").click(function(){
         deselectAll();
         selectCol($(this).attr('id').substring(3, ));
+        alert('asmt clicked');
        
     });
 	$("td").click(function () {
@@ -32,4 +72,6 @@ $(document).ready(function() {
 			}
 		});
 	});
+
+    
 });
